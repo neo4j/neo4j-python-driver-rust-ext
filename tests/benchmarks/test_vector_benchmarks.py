a1904d3778867bcd38a75ccc3f6d7aeab57dedbb
Robsdedude <rouven.bauer@neo4j.com>
2025-11-07 14:59:49 +0100

    Remove unused vector to/from native conversion functions (#70)

--- tests/benchmarks/test_vector_benchmarks.py
+++ tests/benchmarks/test_vector_benchmarks.py
@@ -21,7 +21,6 @@ import pytest
 from ..vector.from_driver.test_vector import (
     _mock_mask_extensions,
     _swap_endian,
-    Vector,
 )
 
 
@@ -34,35 +33,3 @@ def test_bench_swap_endian(benchmark, mocker, ext, type_size, length):
     rounds = max(min(1_000_000 // length, 100_000), 100)
 
     benchmark.pedantic(lambda: _swap_endian(type_size, data), rounds=rounds)
-
-
-@pytest.mark.parametrize("ext", ("numpy", "rust", "python"))
-@pytest.mark.parametrize("dtype", ("i8", "i16", "i32", "i64", "f32", "f64"))
-@pytest.mark.parametrize("as_gen", (True, False))
-@pytest.mark.parametrize("length", (1, 1_000))
-def test_bench_from_native(benchmark, mocker, ext, dtype, as_gen, length):
-    raw_data = bytes(i % 256 for i in range(8 * length))
-    data = Vector.from_bytes(raw_data, dtype).to_native()
-    rounds = max(min(1_000_000 // length, 100_000), 100)
-    _mock_mask_extensions(mocker, ext)
-    if as_gen:
-
-        def work():
-            Vector.from_native((x for x in data), dtype)
-    else:
-
-        def work():
-            Vector.from_native(data, dtype)
-
-    benchmark.pedantic(work, rounds=rounds)
-
-
-@pytest.mark.parametrize("ext", ("numpy", "rust", "python"))
-@pytest.mark.parametrize("dtype", ("i8", "i16", "i32", "i64", "f32", "f64"))
-@pytest.mark.parametrize("length", (1, 1_000))
-def test_bench_to_native(benchmark, mocker, ext, dtype, length):
-    data = Vector.from_bytes(bytes(i % 256 for i in range(8 * length)), dtype)
-    rounds = max(min(1_000_000 // length, 100_000), 100)
-    _mock_mask_extensions(mocker, ext)
-
-    benchmark.pedantic(data.to_native, rounds=rounds)
